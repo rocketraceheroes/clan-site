0a724bacaa5d53e07d5c7cd680bd5ce46af6d609
Josh Arnold <jjarnold@brotherhoodmutual.com>
2014-11-09 22:41:13 -0500

    fix videos request

--- assets/js/dvr.js
+++ assets/js/dvr.js
@@ -5,7 +5,18 @@ $(function(){
 			var dvr_html = '<div class="row">';
 	    $.get('https://agile-mesa-1935.herokuapp.com/xbox_dvr/videos', {gamertag: gamertag}, function(resp){
         $.each(resp.videos, function(index, vid){
-          dvr_html += '<div class="col-xs-6 col-md-4"><a href="#" class="thumbnail"><img src="'+vid.Thumbnail+'" alt="..."></a></div>'
+          dvr_html += [
+          	'<div class="col-xs-6 col-md-4">',
+          	'  <div class="thumbnail">',
+          	'    <a href="#" title="Click to Watch">',
+          	'      <img src="'+vid.Thumbnail+'" alt="...">',
+          	'    </a>',
+          	'    <div class="caption">',
+          	'      <p><b>Game:</b> '+vid.TitleName+' - '+vid.UploadTime+'</p>',
+          	'    </div>',
+          	'  </div>',
+          	'</div>'
+          ].join("\n");
         });
         dvr_html += '</div>'
         $('div.dvr').append(dvr_html);
