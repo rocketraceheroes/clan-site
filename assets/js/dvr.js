080ca885aaa3016a84c2862b6a98011994b6a955
Josh Arnold <jjarnold@brotherhoodmutual.com>
2014-11-09 22:18:47 -0500

    fix videos request

--- assets/js/dvr.js
+++ assets/js/dvr.js
@@ -5,7 +5,7 @@ $(function(){
 			var dvr_html = '';
 	    $.get('https://agile-mesa-1935.herokuapp.com/xbox_dvr/videos', {gamertag: gamertag}, function(resp){
         $.each(resp.videos, function(vid){
-          dvr_html += '<div><a href="'+vid.ClipUri+'"><img src="'+vid.Preview+'"/></a></div>'
+          dvr_html += '<div><a href="'+vid['ClipUri']+'"><img src="'+vid.['Preview']+'"/></a></div>'
         });
         $('div.dvr').append(dvr_html);
 	    });
