257b5416eb8511d7ce73b21e01a10b9e143396b4
Josh Arnold <jjarnold@brotherhoodmutual.com>
2014-11-09 20:07:03 -0500

    wip

--- assets/js/dvr.js
+++ assets/js/dvr.js
@@ -1,12 +1,14 @@
 $(function(){
 	var gamertag = $('#member-gamertag').text();
-	var dvr_html = '';
-	setTimeout(function(){
+	if (gamertag.length){
+		setTimeout(function(){
+			var dvr_html = '';
 	    $.get('https://agile-mesa-1935.herokuapp.com/xbox_dvr/videos', {gamertag: gamertag}, function(resp){
-	        resp.videos.each(function(vid){
-	            dvr_html += '<div><a href="'+vid.ClipUri+'"><img src="'+vid.Preview+'"/></a></div>'
-	        });
-	        $('div.dvr').append(dvr_html);
+        resp.videos.each(function(vid){
+          dvr_html += '<div><a href="'+vid.ClipUri+'"><img src="'+vid.Preview+'"/></a></div>'
+        });
+        $('div.dvr').append(dvr_html);
 	    });
-	}, 1000);
+		}, 1000);
+	}
 });
